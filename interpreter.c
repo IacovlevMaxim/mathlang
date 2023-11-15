f4cb9acaf4b5f3dce7ec0e97f46babb3b2725c0d
IacovlevMaxim <iacovlev.maxim@gmail.com>
2023-11-15 21:01:59 +0200

    Added mul operation to interpreter.c

--- interpreter.c
+++ interpreter.c
@@ -163,55 +163,37 @@ node_t* sub(sstack_t *params, int debug) {
     return add(params, debug);
 }
 
-//node_t* add(sstack_t *params, int debug) {
-//    if(debug) printf("Started add\n");
-//    node_t *to = pop_node(params);
-//    printf("%u\n", to);
-//    node_t *from = pop_node(params);
-//    printf("%u\n", from);
-//
-//
-//    if(debug) printf("got nodes\n");
-//    if(to == NULL) {
-//        fprintf(stderr, "Interpreter error: no target variable in add operation\n");
-//        exit(1);
-//    }
-//
-//    if(from == NULL) {
-//        fprintf(stderr, "Interpreter error: no source variable/value in add operation\n");
-//        exit(1);
-//    }
-//    if(debug) printf("done null checking\n");
-//
-//    if(from->tok_type == INT && to->tok_type == INT) {
-//        if(debug) printf("both are ints\n");
-//        *from->val->i += *to->val->i;
-//        free(to);
-//        return from;
-//    }
-//    printf("aaaa\n");
-//    if(debug) printf("trying to declare floats*\n");
-//    printf("aaaa\n");
-//
-//    if(from->val->f == NULL) {
-//        if(debug) printf("from_f is null (converting to float from int)\n");
-//        float tt = (float)*from->val->i;
-//        *from->val->f = tt;
-//    }
-//    printf("aaaa\n");
-//
-//    if(to->val->f == NULL) {
-//        if(debug) printf("to_f is null (adding its int val to itself)\n");
-//        *from->val->f += (float) *to->val->i;
-//    } else {
-//        if(debug) printf("to_f is null (adding its int val to itself)\n");
-//        *from->val->f += *to->val->f;
-//    }
-//    printf("aaaa\n");
-//
-//    free(to);
-//    return from;
-//}
+node_t* mul(sstack_t *params, int debug) {
+    if(debug) printf("Started mul\n");
+    node_t *to = pop_node(params);
+    node_t *from = pop_node(params);
+
+    if(debug) printf("Popped nodes\n");
+    node_t *res = init_node();
+    res->tok_class = VALUE;
+
+    if(debug) printf("Created res node\n");
+    if(to->tok_type == INT && from->tok_type == INT) {
+        if(debug) printf("int int\n");
+        res->tok_type = INT;
+        res->val->i = to->val->i * from->val->i;
+    } else if(to->tok_type == FLOAT || from->tok_type == FLOAT) {
+        if(debug) {
+            printf("float int\n");
+            printf("to f: %f\n", get_float_value(to) );
+            printf("from int: %f\n", get_float_value(from));
+        }
+
+        res->tok_type = FLOAT;
+        res->val->f = get_float_value(to) * get_float_value(from);
+        if(debug) printf("res f: %f\n", res->val->f);
+    } else {
+        fprintf(stderr, "Interpreter error: invalid argument types (neither floats nor ints)\n");
+        exit(1);
+    }
+
+    return res;
+}
 //Stack example:
 // 1 a asg 1.23 b asg if 1 b gt { while 10 a eq not 1 a add a asg }
 // ^
@@ -246,6 +228,12 @@ void interpret(sstack_t* stack, var **variables, int debug) {
                 node_t *sub_res = sub(new_stack, 1);
                 push_node(new_stack, sub_res);
                 printf("dec res value: %f\n", pop_node(new_stack)->val->f);
+                break;
+            case MUL:
+                if(debug) printf("sub operation\n");
+                node_t *mul_res = mul(new_stack, 1);
+                push_node(new_stack, mul_res);
+                printf("mul res value: %f\n", pop_node(new_stack)->val->f);
                 break;
             default:
                 printf("Operation is not supported\n");
