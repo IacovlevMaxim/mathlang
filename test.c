8188c74754ef01c97ce9ba60e82da917ba9fb7fd
Andre Chicu <andriesh.chicu@gmail.com>
2023-11-21 14:47:51 +0200

    simplpify test.c

--- test.c
+++ test.c
@@ -32,137 +32,15 @@ void input_code(char **code) {
   }
 }
 
-// int main() {
-//     printf("Started main\n");
-//     var a_var = {
-//            "a",
-//            1,
-//            {
-//                 -1
-//            }
-//     };
-//     var* variables = malloc(sizeof(var) * 4);
-//     variables[0] = a_var;
-//     sstack_t* stack = init_stack();
-//
-//     node_t* one = init_node();
-//     one->tok_class = VALUE;
-//     one->tok_type = INT;
-//     one->str = "1";
-//     one->val->i = 1;
-//
-//     node_t* a = init_node();
-//     a->tok_class = ID;
-//     a->tok_type = FLOAT;
-//     a->str = "a";
-//     a->val->f = a_var.value.f;
-//
-//
-////    Testing asg
-//    node_t *asg = init_node();
-//    asg->tok_class = OPERATION;
-//    asg->tok_type = ASG;
-//    asg->str = "asg";
-//
-////  Testing add
-////    node_t *add = init_node();
-////    add->tok_class = OPERATION;
-////    add->tok_type = ADD;
-////    add->str = "add";
-//
-//    //Update next here
-//    a->next = asg;
-//    one->next = a;
-//    stack->node = one;
-//
-//    printf("Finished generating stuff\n");
-//    interpret(stack, &variables, 1);
-//
-//    int i = 0;
-//    printf("Variables:\n");
-//    var* currVar;
-//    while((currVar = (variables + i))->type) {
-//        printf("%s: %d with val ", currVar->name, currVar->type);
-//        if(currVar->type == INT) {
-//            printf("%d\n", currVar->value.i);
-//        } else if(currVar->type == FLOAT) {
-//            printf("%f\n", currVar->value.f);
-//        } else {
-//            printf("No type for variable!");
-//            exit(1);
-//        }
-//        i++;
-//    }
-//    return 0;
-//}
-
-///// b a div
-///// (div): a, b
-
 int main() {
-  // char *code = malloc(sizeof(char) * 256);
-  // input_code(&code);
-  // char *code = "int a\n"
-  //             "float b\n"
-  //             "{\n"
-  //             "\n\n"
-  //             "  asg a 1\n"
-  //             "  if gt a 0 {\n"
-  //             "    asg b 1.23\n"
-  //             "      while lt a 5 {\n"
-  //             "        asg a add a 1\n"
-  //             "      }\n"
-  //             "    asg a add b 2\n"
-  //             "  }\n"
-  //             "  else asg a 2\n"
-  //             "}\n";
-  char *code = "int a\n"
-               "float b\n"
-               "{\n"
-               "\n\n"
-               "  asg a 1\n"
-               "  asg b add a 2.3\n"
-               "}\n";
+  char *code = malloc(sizeof(char) * 256);
+  input_code(&code);
 
-  // printf("Code:\n///\n%s///\n", code);
-  //    node_t node = EMPTY_DATA;
   sstack_t *stack = init_stack();
 
   // After using variables, use free(variables);
   var *variables = malloc(sizeof(var) * MAX_VAR_AMOUNT);
   tokenize(code, stack, &variables, 0);
-
-  //    node_t *stack_curr = stack->node;
-  //
-  //    while(stack_curr != NULL) {
-  //        printf("class: %d\n", stack_curr->tok_class);
-  //        printf("type: %d\n", stack_curr->tok_type);
-  //        printf("str: %s\n", stack_curr->str);
-  //        printf("------\n");
-  //
-  //        stack_curr = stack_curr->next;
-  ////      token_class_t tok_class;
-  ////      token_type_t tok_type;
-  ////      char *str;
-  ////      // size_t no_line;
-  ////      node_val_t *val;
-  //    }
-
-  //  int i = 0;
-  //  printf("Variables:\n");
-  //  var* currVar;
-  //  while((currVar = (variables + i))->type) {
-  //      printf("%s: %d with val ", currVar->name, currVar->type);
-  //      if(currVar->type == INT) {
-  //          printf("%d\n", currVar->value.i);
-  //      } else if(currVar->type == FLOAT) {
-  //          printf("%f\n", currVar->value.f);
-  //      } else {
-  //          printf("No type for variable!");
-  //          exit(1);
-  //      }
-  //      i++;
-  //  }
 
   // After using nodes, use free(node) for each node
   sstack_t *caveman_tree = parse_tokens(stack, 0);
@@ -171,13 +49,5 @@ int main() {
     exit(1);
   }
 
-  interpret(caveman_tree, &variables, 1);
-
-  // printf("Parsed:\n");
-  // node_t *curr = pop_node(caveman_tree);
-  // while (curr != NULL) {
-  //   printf("%s ", curr->str);
-  //   curr = pop_node(caveman_tree);
-  // }
-  // printf("\n");
+   interpret(caveman_tree, &variables, 1);
 }
